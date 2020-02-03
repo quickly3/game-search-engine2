9fb48fcf4fbfd64fc9feb9912785defcacc543aa
HongBin Zhou <hongbin@inceptionpad.com>
2020-02-03 11:53:09 +0800

    upload

--- src/app/infoq/infoq.component.ts
+++ src/app/infoq/infoq.component.ts
@@ -1,7 +1,9 @@
 import { Component, ViewEncapsulation } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
-import { Observable, of, Subject } from "rxjs";
+import { Observable, of, Subject, fromEvent } from "rxjs";
+import { scan } from "rxjs/operators";
+
 import {
     debounceTime,
     distinctUntilChanged,
@@ -91,6 +93,30 @@ export class InfoqComponent {
     ngOnInit(): void {
         this.getWordsCloud();
         this.search();
+
+        const observable = new Observable(subscriber => {
+            subscriber.next(1);
+            subscriber.next(2);
+            subscriber.next(3);
+            setTimeout(() => {
+                subscriber.next(4);
+                subscriber.complete();
+            }, 1000);
+        });
+
+        console.log("just before subscribe");
+        observable.subscribe({
+            next(x) {
+                console.log("got value " + x);
+            },
+            error(err) {
+                console.error("something wrong occurred: " + err);
+            },
+            complete() {
+                console.log("done");
+            }
+        });
+        console.log("just after subscribe");
     }
     showValue(item): void {}
 
