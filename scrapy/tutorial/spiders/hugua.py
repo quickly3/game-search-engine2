459b8ee877219e0913b87f34e903b6f08d2084ac
HongBin Zhou <hongbin@inceptionpad.com>
2020-02-19 00:32:25 +0800

    hugua spider

---
+++ scrapy/tutorial/spiders/hugua.py
@@ -0,0 +1,61 @@
+# -*- coding:UTF-8 -*-
+#
+#
+
+from elasticsearch import Elasticsearch
+from pathlib import Path
+from dotenv import load_dotenv
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from operator import itemgetter
+from itertools import groupby
+import scrapy
+import sys
+import sqlalchemy
+import os
+import re
+import datetime
+
+
+# settings.py
+
+
+es = Elasticsearch()
+
+
+def cont_filter(x):
+    return x.replace("、", ".").replace("\n", "").strip(" ") != ""
+
+
+def distinct(items):
+    key = itemgetter('link')
+    items = sorted(items, key=key)
+    return [next(v) for _, v in groupby(items, key=key)]
+
+
+class AliSpider(scrapy.Spider):
+    # 593
+    name = "hugua"
+    start_urls = [
+        'http://www.hugua.cc/dongman/185770/',
+    ]
+
+    page_list = []
+
+    source = "escn"
+    tag = "elastic"
+
+    def parse(self, response):
+        url = response.css('.downurl>script::text').get()
+        url = url.replace("var GvodUrls5 =", "")
+        url = url.replace("echoDown(GvodUrls5,5)", "")
+        url = url.replace(";", "")
+        url = url.replace("\"", "")
+
+        urls = url.split("###")
+        for thunder_url in urls:
+            line = thunder_url.split("$")
+            if thunder_url != "":
+                print(line[1])
