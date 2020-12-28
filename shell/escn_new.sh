82919187a8784b0288cd80f1fb37ae10c628669c
HongBin Zhou <hongbin@inceptionpad.com>
2020-12-28 18:02:25 +0800

    update

--- shell/escn_new.sh
+++ shell/escn_new.sh
@@ -3,3 +3,4 @@ export PATH=$PATH:/usr/local/bin
 
 cd /home/ubuntu/www/ng-blog/scrapy
 nohup python3 -m scrapy crawl escn_new >> scrapy.log 2>&1
+nohup python3 -m scrapy crawl jianshu_daily >> scrapy.log 2>&1
