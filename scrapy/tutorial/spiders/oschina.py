fa663e1fc595ea6f81b569c3c128ef751ee1ca33
HongBin Zhou <hongbin@inceptionpad.com>
2021-01-18 20:49:34 +0800

    update

--- scrapy/tutorial/spiders/oschina.py
+++ scrapy/tutorial/spiders/oschina.py
@@ -61,21 +61,21 @@ class AliSpider(scrapy.Spider):
     tag = "python"
 
     urlTmpl = Template(
-        'https://www.oschina.net/search?scope=blog&q=${tagId}&onlyme=0&onlytitle=0&sort_by_time=1&p=${page}')
+        'https://www.oschina.net/search?scope=blog&q=${tagId}&onlyme=0&onlytitle=0&sort_by_time=1&p=${page}&_pjax=.search-container')
 
     page = 1
 
     headers = {
         "Accept": "text/html, */*; q=0.01",
         "Accept-Encoding": "gzip, deflate, br",
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/72.0",
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36",
         "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
         "Accept-Language": "en,zh-CN;q=0.9,zh;q=0.8,zh-TW;q=0.7",
         "Cache-Control": "no-cache",
         "Connection": "keep-alive",
         "Host": "www.oschina.net",
         "Pragma": "no-cache",
-        "Referer": "https://www.oschina.net/search?scope=blog&q=python&onlyme=0&onlytitle=0&sort_by_time=1&p=1",
+        "Referer": "https://www.oschina.net/search?scope=blog&q=python&onlyme=0&onlytitle=0&sort_by_time=1&p=2",
         "Sec-Fetch-Dest": "empty",
         "Sec-Fetch-Mode": "cors",
         "Sec-Fetch-Site": "same-origin",
@@ -84,10 +84,8 @@ class AliSpider(scrapy.Spider):
         "X-Requested-With": "XMLHttpRequest",
         "Cookie": "_ga=GA1.2.186448612.1576823180; __gads=ID=9759499603a966d2:T=1587006988:S=ALNI_MaHUCvHBDV4_9p3hvYSCpQ0OHpY1A; _user_behavior_=7c8e975a-2707-44d8-93af-28d6d29010d4; Hm_lvt_a411c4d1664dd70048ee98afe7b28f0b=1610426992; Hm_lpvt_a411c4d1664dd70048ee98afe7b28f0b=1610965672"
     }
-
+    
     def start_requests(self):
-
-
 
         self.tar_arr = []
 
