56781fc44e4882038fcbbb9c4e1799a6155a70aa
HongBin Zhou <hongbin@inceptionpad.com>
2021-01-18 21:33:15 +0800

    update

--- scrapy/tutorial/spiders/oschina.py
+++ scrapy/tutorial/spiders/oschina.py
@@ -57,12 +57,12 @@ class AliSpider(scrapy.Spider):
         "typescript": "typescript",
         "blockchain": "区块链",
         "game": "游戏",
-        "security": "安全",
-        "postgresql": "postgresql",
-        "linux": "linux",
-        "dp": "设计模式",
-        "design": "架构",
-        "algorithm": "算法",
+        # "security": "安全",
+        # "postgresql": "postgresql",
+        # "linux": "linux",
+        # "dp": "设计模式",
+        # "design": "架构",
+        # "algorithm": "算法",
     }
 
     tag = "python"
