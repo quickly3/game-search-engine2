c53d10b05a6922029735117f228fa2af6e322f08
HongBin Zhou <hongbin@inceptionpad.com>
2021-04-01 15:09:44 +0800

    test

--- puppeteer/google_linkedin.js
+++ puppeteer/google_linkedin.js
@@ -23,40 +23,40 @@ const ScrapePage = async (url) => {
         waitUntil: 'networkidle2'
     });
 
-    link = '#content_left > div:nth-child(1)  > h3 > a'
+    link = '#rso > div:nth-child(1) > div:nth-child(1) > div > div:nth-child(1) > a > h3'
 
     console.log(link)
     await page.waitForSelector(link)
-    await page.click(link)
+    
+    const linkNode = await page.$(link); 
+    const newPagePromise = new Promise(x => browser.once('targetcreated', target => x(target.page())));
+    await linkNode.click({button: 'middle'});    
+    const page2 = await newPagePromise;  
+    await page2.bringToFront();     
 
-    page.on('popup', async () => {
-        pages = await browser.pages();
-        if (pages.length == 3) {
-            page2 = pages[2]
-            console.log(page2.url())
-            nameSelector = '#main-content > section.core-rail > section.top-card-layout > div > div.top-card-layout__entity-info-container > div:nth-child(1) > h1'
-            await page2.waitForSelector(nameSelector, {
-                timeout: 10000
-            })
-
-            page2.on('console', msg => {
-                for (let i = 0; i < msg.args.length; i++)[
-                    console.log(`${i}:${msg.args[i]}`)
-                ]
-            })
-            const resp = await page2.evaluate(() => {
-                nameSelector = '#main-content > section.core-rail > section.top-card-layout > div > div.top-card-layout__entity-info-container > div:nth-child(1) > h1'
-                companyName = document.querySelector(nameSelector).textContent
-                return {
-                    companyName
-                };
-            })
-            console.log(page2.url())
-            console.log(resp)
-            await browser.close();
-
-        }
+    console.log(page2.url())
+    nameSelector = '#main-content > section.core-rail > section.top-card-layout > div > div.top-card-layout__entity-info-container > div:nth-child(1) > h1'
+    await page2.waitForSelector(nameSelector, {
+        timeout: 10000
     })
+
+    page2.on('console', msg => {
+        for (let i = 0; i < msg.args.length; i++)[
+            console.log(`${i}:${msg.args[i]}`)
+        ]
+    })
+    const resp = await page2.evaluate(() => {
+        nameSelector = '#main-content > section.core-rail > section.top-card-layout > div > div.top-card-layout__entity-info-container > div:nth-child(1) > h1'
+        companyName = document.querySelector(nameSelector).textContent
+        return {
+            companyName
+        };
+    })
+    console.log(page2.url())
+    console.log(resp)
+    await browser.close();
+
+
 }
 url = "https://www.google.com/search?q=site:linkedin.com+inceptionpad";
 ScrapePage(url);
