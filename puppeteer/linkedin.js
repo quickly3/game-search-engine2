35e34d8b75c636909716c6744adfc0781f1789ef
HongBin Zhou <hongbin@inceptionpad.com>
2021-04-01 17:16:28 +0800

    test

--- puppeteer/linkedin.js
+++ puppeteer/linkedin.js
@@ -15,7 +15,7 @@ const ScrapePage = async(url)=>{
 
     const browser = await puppeteer.launch({
         headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox', '--single-process']
+        args: ['--disable-gpu']
     });
     console.log(1)
 
