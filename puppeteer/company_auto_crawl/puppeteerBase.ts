af87fff59ca8d39a24d8c339435b0493c4015a27
HongBin Zhou <hongbin@inceptionpad.com>
2021-08-18 17:04:37 +0800

    update

--- puppeteer/company_auto_crawl/puppeteerBase.ts
+++ puppeteer/company_auto_crawl/puppeteerBase.ts
@@ -3,7 +3,7 @@ import * as puppeteer from "puppeteer"
 export default class PuppeteerBase{
     browser:any;
 
-    async init (headless=true){
+    async init (headless=false){
 
         const userAgent = 'Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11'
 
