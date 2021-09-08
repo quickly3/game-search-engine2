3b178af2767ef698bfa4dd4d3f1b8fa00e0ecc50
HongBin Zhou <hongbin@inceptionpad.com>
2021-09-08 11:36:17 +0800

    update

--- scrapy/update/juejin_author.sh
+++ scrapy/update/juejin_author.sh
@@ -3,4 +3,4 @@ export PATH=$PATH:/usr/local/bin
 
 cd /home/ubuntu/www/ng-blog/scrapy/update
 
-nohup python3 -m juejin_author_crawl.py >> juejin_author_crawl.log 2>&1
+nohup python3 -m juejin_author_crawl >> juejin_author_crawl.log 2>&1
