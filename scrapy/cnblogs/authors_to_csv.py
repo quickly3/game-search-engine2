b90d08b9ff10660b0cae670b5daa9226115a4eb3
Bean <quickly3@sohu.com>
2022-01-09 15:21:39 +0800

    update

--- scrapy/cnblogs/authors_to_csv.py
+++ scrapy/cnblogs/authors_to_csv.py
@@ -1,18 +1,22 @@
 # 导出全部juejin文章作者
+from sys import path
+path.append(r"../es_services");
+
 from es_client import EsClient
 import csv
 import os
 
 if __name__ == "__main__":
-
     file = 'authors.csv'
 
     if os.path.exists(file):
         os.remove(file)
 
     es = EsClient()
+    es.setQuerySource('cnblogs');
     toCSV = es.getAuthors()
     toCSV = sorted(toCSV, key=lambda k: k['doc_count'], reverse=True)
+    print(toCSV)
 
     keys = toCSV[0].keys()
 
@@ -20,4 +24,3 @@ if __name__ == "__main__":
         dict_writer = csv.DictWriter(output_file, keys)
         dict_writer.writeheader()
         dict_writer.writerows(toCSV)
-
