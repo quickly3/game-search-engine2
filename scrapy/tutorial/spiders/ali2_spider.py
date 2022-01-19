2fe343b35d14d155ed062c01f7ed303a16c6cb58
Hongbin Zhou <quickly3@sohu.com>
2022-01-19 15:21:58 +0800

    update

--- scrapy/tutorial/spiders/ali2_spider.py
+++ scrapy/tutorial/spiders/ali2_spider.py
@@ -1,248 +1,248 @@
-# -*- coding:UTF-8 -*-
-#
-#
+# # -*- coding:UTF-8 -*-
+# #
+# #
 
-import scrapy
-import sys
-import sqlalchemy
-import os
-import json
-import re
+# import scrapy
+# import sys
+# import sqlalchemy
+# import os
+# import json
+# import re
 
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Text, text
-from sqlalchemy.orm import sessionmaker
+# from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy import Column, Integer, String, Text, text
+# from sqlalchemy.orm import sessionmaker
 
-from urllib.parse import urlparse, parse_qs
-
-
-# settings.py
-from dotenv import load_dotenv
-from pathlib import Path
-env_path = Path('..')/'.env'
-load_dotenv(dotenv_path=env_path)
-
-DB_DATABASE = os.getenv("DB_DATABASE")
-DB_USERNAME = os.getenv("DB_USERNAME")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-
-engine = create_engine("mysql+pymysql://"+DB_USERNAME+":"+DB_PASSWORD +
-                       "@localhost/"+DB_DATABASE+"?charset=utf8", encoding='utf-8', echo=False)
-
-Base = declarative_base()
+# from urllib.parse import urlparse, parse_qs
 
 
-Session_class = sessionmaker(bind=engine)
-Session = Session_class()
+# # settings.py
+# from dotenv import load_dotenv
+# from pathlib import Path
+# env_path = Path('..')/'.env'
+# load_dotenv(dotenv_path=env_path)
 
-dr = re.compile(r'<[^>]+>', re.S)
+# DB_DATABASE = os.getenv("DB_DATABASE")
+# DB_USERNAME = os.getenv("DB_USERNAME")
+# DB_PASSWORD = os.getenv("DB_PASSWORD")
 
+# engine = create_engine("mysql+pymysql://"+DB_USERNAME+":"+DB_PASSWORD +
+#                        "@localhost/"+DB_DATABASE+"?charset=utf8", encoding='utf-8', echo=False)
 
-class Game(Base):
-    __tablename__ = 'games'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(200))
-    image_url = Column(String(200))
-    image_alt = Column(String(200))
-    version = Column(String(200))
-    size = Column(String(50))
-    detail_page = Column(String(200))
-
-    appid = Column(Integer)
-    conid = Column(Integer)
-    download_page = Column(String(200))
-    name_chs = Column(String(200))
-    inLanguage = Column(String(200))
-    license = Column(String(200))
-    name_en = Column(String(200))
-    game_type = Column(String(200))
-    publisher = Column(String(200))
-    publish_date_by_ali = Column(String(200))
-    game_tags_string = Column(Text)
-    game_images_mini_string = Column(Text)
-    game_images_string = Column(Text)
-    description = Column(Text)
-    sys_requirements = Column(Text)
-    install_info = Column(Text)
-    soft_requirements = Column(Text)
-
-    state = Column(Integer)
+# Base = declarative_base()
 
 
-class Ali2Spider(scrapy.Spider):
-    # 593
-    name = "ali2"
-    current_id = 1
-    download_link = "http://www.soft5566.com/down/{ali_id}-1.html"
+# Session_class = sessionmaker(bind=engine)
+# Session = Session_class()
 
-    # start_urls = [
-    #     'http://down.ali213.net/pcgame/all/0-0-0-0-new-pic-604.html'
-    #     # 'http://down.ali213.net/pcgame/',
-    # ]
+# dr = re.compile(r'<[^>]+>', re.S)
 
-    def start_requests(self):
-        # game_obj = Game(name=name, image_url=image_url, image_alt=image_alt, version=version, size=size, detail_page=detail_page)
-        game = Session.query(Game).filter(
-            text("state=0")).order_by(Game.id).first()
 
-        self.current_id = game.id
-        #
-        yield scrapy.Request(game.detail_page, self.parse)
+# class Game(Base):
+#     __tablename__ = 'games'
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String(200))
+#     image_url = Column(String(200))
+#     image_alt = Column(String(200))
+#     version = Column(String(200))
+#     size = Column(String(50))
+#     detail_page = Column(String(200))
 
-        # self.current_data = game;
+#     appid = Column(Integer)
+#     conid = Column(Integer)
+#     download_page = Column(String(200))
+#     name_chs = Column(String(200))
+#     inLanguage = Column(String(200))
+#     license = Column(String(200))
+#     name_en = Column(String(200))
+#     game_type = Column(String(200))
+#     publisher = Column(String(200))
+#     publish_date_by_ali = Column(String(200))
+#     game_tags_string = Column(Text)
+#     game_images_mini_string = Column(Text)
+#     game_images_string = Column(Text)
+#     description = Column(Text)
+#     sys_requirements = Column(Text)
+#     install_info = Column(Text)
+#     soft_requirements = Column(Text)
 
-    def parse(self, response):
+#     state = Column(Integer)
 
-        id_zone_url = response.css(
-            '#ali_comment_pc_global::attr(src)').extract_first()
 
-        if id_zone_url != None:
+# class Ali2Spider(scrapy.Spider):
+#     # 593
+#     name = "ali2"
+#     current_id = 1
+#     download_link = "http://www.soft5566.com/down/{ali_id}-1.html"
 
-            query = urlparse(id_zone_url).query
-            params = dict([(k, v[0]) for k, v in parse_qs(query).items()])
+#     # start_urls = [
+#     #     'http://down.ali213.net/pcgame/all/0-0-0-0-new-pic-604.html'
+#     #     # 'http://down.ali213.net/pcgame/',
+#     # ]
 
-            game_ids = params['sid'].split("-")
+#     def start_requests(self):
+#         # game_obj = Game(name=name, image_url=image_url, image_alt=image_alt, version=version, size=size, detail_page=detail_page)
+#         game = Session.query(Game).filter(
+#             text("state=0")).order_by(Game.id).first()
 
-            appid = game_ids[0]
-            conid = game_ids[1]
-        else:
-            id_zone_url = response.css(
-                '#ali_hits_pc_global::attr(src)').extract_first()
+#         self.current_id = game.id
+#         #
+#         yield scrapy.Request(game.detail_page, self.parse)
 
-            query = urlparse(id_zone_url).query
-            params = dict([(k, v[0]) for k, v in parse_qs(query).items()])
+#         # self.current_data = game;
 
-            appid = params['entityID']
-            conid = params['channelID']
+#     def parse(self, response):
 
-        download_page = self.download_link.replace("{ali_id}", conid)
+#         id_zone_url = response.css(
+#             '#ali_comment_pc_global::attr(src)').extract_first()
 
-        name_chs = response.css(
-            '.detail_game_l_r_ctit h1::text').extract_first()
-        name_chs = dr.sub('', name_chs)
+#         if id_zone_url != None:
 
-        inLanguage = response.css(
-            '.detail_game_l_r_ctit span[itemprop=inLanguage]::text').extract_first()
-        license = response.css(
-            '.detail_game_l_r_ctit span[itemprop=license]::text').extract_first()
-        name_en = response.css(
-            '.detail_game_l_r_ctit span[itemprop=alias]::text').extract_first()
+#             query = urlparse(id_zone_url).query
+#             params = dict([(k, v[0]) for k, v in parse_qs(query).items()])
 
-        game_type = response.css('.detail_game_l_r_info ul li')[
-            0].css("a::text").extract_first()
+#             game_ids = params['sid'].split("-")
 
-        publish_date_by_ali = response.css('.detail_game_l_r_info ul li')[
-            1].css("a span::text").extract_first()
+#             appid = game_ids[0]
+#             conid = game_ids[1]
+#         else:
+#             id_zone_url = response.css(
+#                 '#ali_hits_pc_global::attr(src)').extract_first()
 
-        publisher = response.css('.detail_game_l_r_info ul li')[
-            2].css("::text").extract_first()
-        publisher = publisher.split("：")[1]
+#             query = urlparse(id_zone_url).query
+#             params = dict([(k, v[0]) for k, v in parse_qs(query).items()])
 
-        game_tags = response.css('.detail_game_l_r_tag a::text').extract()
+#             appid = params['entityID']
+#             conid = params['channelID']
 
-        game_tags_string = ",".join(game_tags)
+#         download_page = self.download_link.replace("{ali_id}", conid)
 
-        game_images_mini_string = ""
-        game_images_mini = response.css(
-            '#smallImg span img::attr(src)').extract()
-        if len(game_images_mini) > 0:
-            game_images_mini.pop()
-            game_images_mini_string = ",".join(game_images_mini)
+#         name_chs = response.css(
+#             '.detail_game_l_r_ctit h1::text').extract_first()
+#         name_chs = dr.sub('', name_chs)
 
-        game_images_string = ""
-        game_images = response.css(
-            '.detail_body_con_bb_con_con .detail_body_con_jt_con_img img::attr(src)').extract()
-        if len(game_images) > 0:
-            game_images.pop()
-            game_images_string = ",".join(game_images)
+#         inLanguage = response.css(
+#             '.detail_game_l_r_ctit span[itemprop=inLanguage]::text').extract_first()
+#         license = response.css(
+#             '.detail_game_l_r_ctit span[itemprop=license]::text').extract_first()
+#         name_en = response.css(
+#             '.detail_game_l_r_ctit span[itemprop=alias]::text').extract_first()
 
-        description = ""
-        #  detail_body_con_bb_con1
-        description_arr = response.css(
-            '#yxjs .detail_body_left_info_con p').extract()
+#         game_type = response.css('.detail_game_l_r_info ul li')[
+#             0].css("a::text").extract_first()
 
-        if len(description_arr) > 0:
-            description = "".join(description_arr)
+#         publish_date_by_ali = response.css('.detail_game_l_r_info ul li')[
+#             1].css("a span::text").extract_first()
 
-        # if description != None:
-        #     description = dr.sub('',description)
+#         publisher = response.css('.detail_game_l_r_info ul li')[
+#             2].css("::text").extract_first()
+#         publisher = publisher.split("：")[1]
 
-        # print description;
-        # os._exit()
+#         game_tags = response.css('.detail_game_l_r_tag a::text').extract()
 
-        sys_requirements = ""
-        install_info = ""
-        soft_requirements = ""
+#         game_tags_string = ",".join(game_tags)
 
-        gameSetting = response.css(
-            '#pzyq .detail_body_left_info_con .gameSetting ul')
+#         game_images_mini_string = ""
+#         game_images_mini = response.css(
+#             '#smallImg span img::attr(src)').extract()
+#         if len(game_images_mini) > 0:
+#             game_images_mini.pop()
+#             game_images_mini_string = ",".join(game_images_mini)
 
-        if len(gameSetting) > 0:
+#         game_images_string = ""
+#         game_images = response.css(
+#             '.detail_body_con_bb_con_con .detail_body_con_jt_con_img img::attr(src)').extract()
+#         if len(game_images) > 0:
+#             game_images.pop()
+#             game_images_string = ",".join(game_images)
 
-            sys_requirements_key = gameSetting[0].css(
-                'li span::text').extract()
-            sys_requirements_value1 = gameSetting[1].css(
-                'li span::text').extract()
-            sys_requirements_value2 = gameSetting[2].css(
-                'li span::text').extract()
+#         description = ""
+#         #  detail_body_con_bb_con1
+#         description_arr = response.css(
+#             '#yxjs .detail_body_left_info_con p').extract()
 
-            sys_requirements = []
+#         if len(description_arr) > 0:
+#             description = "".join(description_arr)
 
-            for i in range(0, len(sys_requirements_key)):
-                item = {}
-                item['key'] = sys_requirements_key[i]
-                item['v1'] = sys_requirements_value1[i]
-                item['v2'] = sys_requirements_value2[i]
-                sys_requirements.append(item)
+#         # if description != None:
+#         #     description = dr.sub('',description)
 
-            sys_requirements = json.dumps(sys_requirements)
+#         # print description;
+#         # os._exit()
 
-        install_info = response.css(
-            '#azsm .detail_body_left_info_con p::text').extract()
-        install_info = ",".join(install_info)
+#         sys_requirements = ""
+#         install_info = ""
+#         soft_requirements = ""
 
-        soft_requirements = response.css('.yunxingku ul li').extract()
+#         gameSetting = response.css(
+#             '#pzyq .detail_body_left_info_con .gameSetting ul')
 
-        soft_requirements = ",".join(soft_requirements)
+#         if len(gameSetting) > 0:
 
-        soft_requirements = soft_requirements.replace('<li>', "")
-        soft_requirements = soft_requirements.replace('</li>', "")
+#             sys_requirements_key = gameSetting[0].css(
+#                 'li span::text').extract()
+#             sys_requirements_value1 = gameSetting[1].css(
+#                 'li span::text').extract()
+#             sys_requirements_value2 = gameSetting[2].css(
+#                 'li span::text').extract()
 
-        update_obj = {
-            Game.state: 1,
-            Game.appid: appid,
-            Game.conid: conid,
-            Game.download_page: download_page,
-            Game.name_chs: name_chs,
-            Game.name_en: name_en,
-            Game.inLanguage: inLanguage,
-            Game.license: license,
-            Game.game_type: game_type,
-            Game.publisher: publisher,
-            Game.publish_date_by_ali: publish_date_by_ali,
-            Game.game_tags_string: game_tags_string,
-            Game.game_images_mini_string: game_images_mini_string,
-            Game.game_images_string: game_images_string,
-            Game.description: description,
-            Game.sys_requirements: sys_requirements,
-            Game.install_info: install_info,
-            Game.soft_requirements: soft_requirements
-        }
+#             sys_requirements = []
 
-        Session.query(Game).filter(
-            Game.id == self.current_id).update(update_obj)
-        Session.commit()
+#             for i in range(0, len(sys_requirements_key)):
+#                 item = {}
+#                 item['key'] = sys_requirements_key[i]
+#                 item['v1'] = sys_requirements_value1[i]
+#                 item['v2'] = sys_requirements_value2[i]
+#                 sys_requirements.append(item)
 
-        # self.current_id += 1;
+#             sys_requirements = json.dumps(sys_requirements)
 
-        try:
-            next_game = Session.query(Game).filter(
-                text("state=0")).order_by(Game.id).first()
-            self.current_id = next_game.id
-            # next_game = Session.query(Game).filter(Game.id == self.current_id).one()
-        except BaseException:
-            print("All thing done.")
-        else:
-            next_page_url = next_game.detail_page + '?_=' + str(next_game.id)
-            yield response.follow(next_page_url, callback=self.parse)
+#         install_info = response.css(
+#             '#azsm .detail_body_left_info_con p::text').extract()
+#         install_info = ",".join(install_info)
+
+#         soft_requirements = response.css('.yunxingku ul li').extract()
+
+#         soft_requirements = ",".join(soft_requirements)
+
+#         soft_requirements = soft_requirements.replace('<li>', "")
+#         soft_requirements = soft_requirements.replace('</li>', "")
+
+#         update_obj = {
+#             Game.state: 1,
+#             Game.appid: appid,
+#             Game.conid: conid,
+#             Game.download_page: download_page,
+#             Game.name_chs: name_chs,
+#             Game.name_en: name_en,
+#             Game.inLanguage: inLanguage,
+#             Game.license: license,
+#             Game.game_type: game_type,
+#             Game.publisher: publisher,
+#             Game.publish_date_by_ali: publish_date_by_ali,
+#             Game.game_tags_string: game_tags_string,
+#             Game.game_images_mini_string: game_images_mini_string,
+#             Game.game_images_string: game_images_string,
+#             Game.description: description,
+#             Game.sys_requirements: sys_requirements,
+#             Game.install_info: install_info,
+#             Game.soft_requirements: soft_requirements
+#         }
+
+#         Session.query(Game).filter(
+#             Game.id == self.current_id).update(update_obj)
+#         Session.commit()
+
+#         # self.current_id += 1;
+
+#         try:
+#             next_game = Session.query(Game).filter(
+#                 text("state=0")).order_by(Game.id).first()
+#             self.current_id = next_game.id
+#             # next_game = Session.query(Game).filter(Game.id == self.current_id).one()
+#         except BaseException:
+#             print("All thing done.")
+#         else:
+#             next_page_url = next_game.detail_page + '?_=' + str(next_game.id)
+#             yield response.follow(next_page_url, callback=self.parse)
